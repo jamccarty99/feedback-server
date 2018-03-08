110754bb7ce2c92c38271ceeab8a79894913614a
Justin McCarty <jamccarty99@gmail.com>
2018-03-08 10:11:32 -0500

    initial heroku add

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    googleClientID: '876260989274-jmkctl7trp7tmro6ompb5mnsj4hgk4r6.apps.googleusercontent.com',
+    googleClientSecret: 'cQ5DG4qazEtv_BED0bywzXwy',
+    mongoURI: 'mongodb://localhost/feedback-loop',
+    cookieKey: 'cookie',
+  };
